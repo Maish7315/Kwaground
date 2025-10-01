3e4f9d90b984a536d027fcae7842b0be40d3ef67
roy sanga <roysa@RoyalRoy.ROY>
2025-10-01 18:27:29 +0300

    Change base to '/' for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',   // 👈 This fixes white screen on Render/Netlify/GitHub Pages
+  base: '/',   // 👈 This fixes white screen on Render/Netlify/GitHub Pages
   build: {
     outDir: 'dist'
   },
