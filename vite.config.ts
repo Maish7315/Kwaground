a8038099d30991a1798bc1b4bddee417534211c5
roy sanga <roysa@RoyalRoy.ROY>
2025-10-01 17:55:06 +0300

    Update vite config with base './' for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
-  base: '/Kwaground/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
   plugins: [react()],
+  base: './',   // 👈 This fixes white screen on Render/Netlify/GitHub Pages
+  build: {
+    outDir: 'dist'
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+})
